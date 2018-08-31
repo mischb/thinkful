92a48c6ecefadef004e9487651a5483ab0a107b4
mischb <mischarberlin@gmail.com>
2018-08-31 13:44:57 -0400

    added instructions and skeleton computeSumBetween

---
+++ computeSumBetween.js
@@ -0,0 +1,21 @@
+
+'use strict';
+
+// Write a function called "computeSumBetween".
+
+// Given 2 integers, "computeSumBetween" returns the sum between the two given integers, beginning at num1, and excluding num2.
+
+// Notes:
+// * The sum between 1 and 4 is 1 + 2 + 3 = 6.
+// * If num2 is not greater than num1, it should return 0.
+
+
+function computeSumBetween(num1, num2) {
+    // your code here
+  }
+
+
+
+
+var output = computeSumBetween(2, 5);
+console.log(output); // --> 9

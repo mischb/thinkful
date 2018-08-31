eb95abc1a4dd9d21e5cdfc130d26a9573fdb12b9
mischb <mischarberlin@gmail.com>
2018-08-31 10:18:26 -0400

    initial commit w instructions'

---
+++ getEvenLengthWordsAtProperty.js
@@ -0,0 +1,19 @@
+// Write a function called "getEvenLengthWordsAtProperty".
+
+// Given an object and a key, "getEvenLengthWordsAtProperty" returns an array containing all the even length word elements of the array located at the given key.
+
+// Notes:
+// * If the array is empty, it should return an empty array.
+// * If it contains no even length elements, it should return an empty array.
+// * If the property at the given key is not an array, it should return an empty array.
+// * If there is no property at the key, it should return an empty array.
+
+'use strict';
+
+function getEvenLengthWordsAtProperty(obj, key) {
+
+  }
+
+function isEven(num){
+    if(num % 2 === 0) return true;
+}

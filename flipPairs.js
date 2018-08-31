7330b247a2468603d0ba43ade19eeba61940f557
mischb <mischarberlin@gmail.com>
2018-08-31 16:13:43 -0400

    added instructions and skeleton flipPairs

---
+++ flipPairs.js
@@ -0,0 +1,13 @@
+// Flip every pair of characters in a string.
+
+// example :
+
+// var input = 'check out how interesting this problem is, it\'s insanely interesting!';
+// var output = flipPairs(input);
+// console.log(output); // --> hcce kuo toh wnietertsni ghtsip orlbmei ,si 't sniasenyli tnreseitgn!
+
+'use strict';
+function flipPairs(input)
+{
+  
+}

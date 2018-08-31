c3eacf0d50237f00b6fd1f113f1da35f140771f1
mischb <mischarberlin@gmail.com>
2018-08-31 13:03:40 -0400

    completed first draft untested

---
+++ filterOddLengthWords.js
@@ -0,0 +1,33 @@
+
+// Write a function called "filterOddLengthWords".Write a function called "filter
+
+// Given an array of string, "filterOddLengthWords" returns an array containing only the elements of the given array whose lengths are odd numbers.
+
+'use strict';
+
+function filterOddLengthWords(words){
+    let returnArray = [];
+    for(let i = 0; i < words.length; i ++){
+        if(isOdd(words[i])){
+            returnArray.push(words[i]);
+        }
+  }
+  return returnArray;
+}
+
+function isOdd(word){
+  return (word.length % 2 !== 0);
+}
+
+
+
+
+
+
+
+
+
+
+
+var output = filterOddLengthWords(['there', 'it', 'is', 'now']);
+console.log(output); // --> ['there', "now']

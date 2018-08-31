c1d17001d7f8403c8a0f645050a7468407df08be
mischb <mischarberlin@gmail.com>
2018-08-31 16:17:43 -0400

    draft flipPairs and new function flipping

--- flipPairs.js
+++ flipPairs.js
@@ -2,12 +2,22 @@
 
 // example :
 
-// var input = 'check out how interesting this problem is, it\'s insanely interesting!';
-// var output = flipPairs(input);
-// console.log(output); // --> hcce kuo toh wnietertsni ghtsip orlbmei ,si 't sniasenyli tnreseitgn!
-
 'use strict';
 function flipPairs(input)
 {
-  
+  let flipped = '';
+  for(let i = 0; i<input.length-2;i++){
+    let flip = flipping(input[i], input[i+1]);
+    flipped + flip;
+  }
+  return flipped;
 }
+
+function flipping(char1, char2){
+  return char2 + char1;
+}
+
+
+var input = 'check out how interesting this problem is, it\'s insanely interesting!';
+var output = flipPairs(input);
+console.log(output); // --> hcce kuo toh wnietertsni ghtsip orlbmei ,si 't sniasenyli tnreseitgn!

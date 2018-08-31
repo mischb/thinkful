c35de9913623dfa6aeb3def3f77e4ab647fc23f1
mischb <mischarberlin@gmail.com>
2018-08-31 17:56:58 -0400

    completed detectOutliers added skeletong for countEvens, countOdd, isEven

--- detectOutlierValue.js
+++ detectOutlierValue.js
@@ -7,10 +7,32 @@
 
 function detectOutlierValue(string)
 {
+  let evens = countEvens(string);
+  let odds = countOdds(string);
+  if (evens > odds){
+    for(let i = 0; i < string.length; i++){
+      if (!isEven(string[i])) return i + 1;
+    }
+  }else {
+    for(let i = 0; i < string.length; i++){
+      if (isEven(string[i])) return i + 1;
+    }
+  }
+}
+
+function countEvens(string){
 
 }
 
+function countOdds(string){
+
+}
+function isEven(char){
+
+}
+
+
 // example:
 
-detectOutlierValue("2 4 7 8 10"); // => 3 - Third number is odd, while the rest of the numbers are even
-detectOutlierValue("1 10 1 1");  //=> 2 - Second number is even, while the rest of the numbers are odd
+detectOutlierValue('2 4 7 8 10'); // => 3 - Third number is odd, while the rest of the numbers are even
+detectOutlierValue('1 10 1 1');  //=> 2 - Second number is even, while the rest of the numbers are odd

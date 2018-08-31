2d3c33da7e27e9683473ff074e127742722ecac2
mischb <mischarberlin@gmail.com>
2018-08-31 16:31:04 -0400

    add instructions and skeleton

---
+++ flipEveryNChars.js
@@ -0,0 +1,21 @@
+// Flip every chunk of n characters in a string, where n is any positive integer greater than 1.
+
+'use strict';
+function flipEveryNChars(input, number)
+{
+  
+}
+
+
+
+
+var input = 'a short example'; 
+var output = flipEveryNChars(input, 5);
+console.log(output); // --> ohs axe trelpma
+
+// Breaking this example down piece by piece:
+// 'a sho' -> 'ohs a'
+// 'rt ex' -> 'xe tr'
+// 'ample' -> 'elpma'
+
+// -> 'ohs axe trelpma'

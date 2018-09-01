252962dcf2f5199d79a419c24e866a83b4a5b8d6
mischb <mischarberlin@gmail.com>
2018-09-01 11:02:21 -0400

    finished passed all tests

--- detectOutlierValue.js
+++ detectOutlierValue.js
@@ -7,32 +7,30 @@
 
 function detectOutlierValue(string)
 {
-  let evens = countEvens(string);
-  let odds = countOdds(string);
+  let array = string.split(" ");
+  let evens = countEvens(array);
+  let odds = array.length - evens;
+  console.log(`evens: ${evens} odd: ${odds}`);
   if (evens > odds){
-    for(let i = 0; i < string.length; i++){
-      if (!isEven(string[i])) return i + 1;
-    }
+    return (array.findIndex(num=> !isEven(num)) + 1);
   }else {
-    for(let i = 0; i < string.length; i++){
-      if (isEven(string[i])) return i + 1;
-    }
+    return (array.findIndex(num=> isEven(num)) + 1);
   }
 }
 
-function countEvens(string){
-
+function countEvens(array){
+  let count = 0;
+  for(let i = 0; i < array.length; i++){
+    if (isEven(array[i])) count ++;
+  }
+  return count;
 }
-
-function countOdds(string){
-
-}
-function isEven(char){
-
+function isEven(num){
+  return num % 2 === 0;  
 }
 
 
 // example:
 
-detectOutlierValue('2 4 7 8 10'); // => 3 - Third number is odd, while the rest of the numbers are even
-detectOutlierValue('1 10 1 1');  //=> 2 - Second number is even, while the rest of the numbers are odd
+console.log(detectOutlierValue("3 5 7 9 11 4")); // => 3 - Third number is odd, while the rest of the numbers are even
+// detectOutlierValue('1 10 1 1');  //=> 2 - Second number is even, while the rest of the numbers are odd
